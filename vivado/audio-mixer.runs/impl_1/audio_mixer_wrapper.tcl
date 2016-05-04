6f6f5bd4f6afd3a41f31a9acfa39123e9d50fe4a
raogbe <raogbe@ttu.ee>
2016-05-04 14:12:26 +0300

    Some updates

--- vivado/audio-mixer.runs/impl_1/audio_mixer_wrapper.tcl
+++ vivado/audio-mixer.runs/impl_1/audio_mixer_wrapper.tcl
@@ -52,23 +52,23 @@ set rc [catch {
   create_project -in_memory -part xc7z020clg484-1
   set_property board_part em.avnet.com:zed:part0:1.3 [current_project]
   set_property design_mode GateLvl [current_fileset]
-  set_property webtalk.parent_dir /home/INTRA/rauno.padari/workspace/soc-audio-mixer/vivado/audio-mixer.cache/wt [current_project]
-  set_property parent.project_path /home/INTRA/rauno.padari/workspace/soc-audio-mixer/vivado/audio-mixer.xpr [current_project]
+  set_property webtalk.parent_dir /home/INTRA/raogbe/workspace/soc-audio-mixer/vivado/audio-mixer.cache/wt [current_project]
+  set_property parent.project_path /home/INTRA/raogbe/workspace/soc-audio-mixer/vivado/audio-mixer.xpr [current_project]
   set_property ip_repo_paths {
-  /home/INTRA/rauno.padari/workspace/soc-audio-mixer/vivado/audio-mixer.cache/ip
-  /home/INTRA/rauno.padari/workspace/soc-audio-mixer/ip_repo/axi_to_audio/axi_to_audio_1.0
-  /home/INTRA/rauno.padari/workspace/soc-audio-mixer/ip_repo/audio_to_axi/audio_to_axi_1.0
-  /home/INTRA/rauno.padari/workspace/soc-audio-mixer/ip_repo/zedboard_audio
+  /home/INTRA/raogbe/workspace/soc-audio-mixer/vivado/audio-mixer.cache/ip
+  /home/INTRA/raogbe/workspace/soc-audio-mixer/ip_repo/axi_to_audio/axi_to_audio_1.0
+  /home/INTRA/raogbe/workspace/soc-audio-mixer/ip_repo/audio_to_axi/audio_to_axi_1.0
+  /home/INTRA/raogbe/workspace/soc-audio-mixer/ip_repo/zedboard_audio
 } [current_project]
-  set_property ip_output_repo /home/INTRA/rauno.padari/workspace/soc-audio-mixer/vivado/audio-mixer.cache/ip [current_project]
-  add_files -quiet /home/INTRA/rauno.padari/workspace/soc-audio-mixer/vivado/audio-mixer.runs/synth_1/audio_mixer_wrapper.dcp
-  read_xdc -ref audio_mixer_processing_system7_0_0 -cells inst /home/INTRA/rauno.padari/workspace/soc-audio-mixer/vivado/audio-mixer.srcs/sources_1/bd/audio_mixer/ip/audio_mixer_processing_system7_0_0/audio_mixer_processing_system7_0_0.xdc
-  set_property processing_order EARLY [get_files /home/INTRA/rauno.padari/workspace/soc-audio-mixer/vivado/audio-mixer.srcs/sources_1/bd/audio_mixer/ip/audio_mixer_processing_system7_0_0/audio_mixer_processing_system7_0_0.xdc]
-  read_xdc -prop_thru_buffers -ref audio_mixer_rst_processing_system7_0_100M_0 /home/INTRA/rauno.padari/workspace/soc-audio-mixer/vivado/audio-mixer.srcs/sources_1/bd/audio_mixer/ip/audio_mixer_rst_processing_system7_0_100M_0/audio_mixer_rst_processing_system7_0_100M_0_board.xdc
-  set_property processing_order EARLY [get_files /home/INTRA/rauno.padari/workspace/soc-audio-mixer/vivado/audio-mixer.srcs/sources_1/bd/audio_mixer/ip/audio_mixer_rst_processing_system7_0_100M_0/audio_mixer_rst_processing_system7_0_100M_0_board.xdc]
-  read_xdc -ref audio_mixer_rst_processing_system7_0_100M_0 /home/INTRA/rauno.padari/workspace/soc-audio-mixer/vivado/audio-mixer.srcs/sources_1/bd/audio_mixer/ip/audio_mixer_rst_processing_system7_0_100M_0/audio_mixer_rst_processing_system7_0_100M_0.xdc
-  set_property processing_order EARLY [get_files /home/INTRA/rauno.padari/workspace/soc-audio-mixer/vivado/audio-mixer.srcs/sources_1/bd/audio_mixer/ip/audio_mixer_rst_processing_system7_0_100M_0/audio_mixer_rst_processing_system7_0_100M_0.xdc]
-  read_xdc /home/INTRA/rauno.padari/workspace/soc-audio-mixer/ip_repo/zedboard_audio/constraints/zed_audio.xdc
+  set_property ip_output_repo /home/INTRA/raogbe/workspace/soc-audio-mixer/vivado/audio-mixer.cache/ip [current_project]
+  add_files -quiet /home/INTRA/raogbe/workspace/soc-audio-mixer/vivado/audio-mixer.runs/synth_1/audio_mixer_wrapper.dcp
+  read_xdc -ref audio_mixer_processing_system7_0_0 -cells inst /home/INTRA/raogbe/workspace/soc-audio-mixer/vivado/audio-mixer.srcs/sources_1/bd/audio_mixer/ip/audio_mixer_processing_system7_0_0/audio_mixer_processing_system7_0_0.xdc
+  set_property processing_order EARLY [get_files /home/INTRA/raogbe/workspace/soc-audio-mixer/vivado/audio-mixer.srcs/sources_1/bd/audio_mixer/ip/audio_mixer_processing_system7_0_0/audio_mixer_processing_system7_0_0.xdc]
+  read_xdc -prop_thru_buffers -ref audio_mixer_rst_processing_system7_0_100M_0 /home/INTRA/raogbe/workspace/soc-audio-mixer/vivado/audio-mixer.srcs/sources_1/bd/audio_mixer/ip/audio_mixer_rst_processing_system7_0_100M_0/audio_mixer_rst_processing_system7_0_100M_0_board.xdc
+  set_property processing_order EARLY [get_files /home/INTRA/raogbe/workspace/soc-audio-mixer/vivado/audio-mixer.srcs/sources_1/bd/audio_mixer/ip/audio_mixer_rst_processing_system7_0_100M_0/audio_mixer_rst_processing_system7_0_100M_0_board.xdc]
+  read_xdc -ref audio_mixer_rst_processing_system7_0_100M_0 /home/INTRA/raogbe/workspace/soc-audio-mixer/vivado/audio-mixer.srcs/sources_1/bd/audio_mixer/ip/audio_mixer_rst_processing_system7_0_100M_0/audio_mixer_rst_processing_system7_0_100M_0.xdc
+  set_property processing_order EARLY [get_files /home/INTRA/raogbe/workspace/soc-audio-mixer/vivado/audio-mixer.srcs/sources_1/bd/audio_mixer/ip/audio_mixer_rst_processing_system7_0_100M_0/audio_mixer_rst_processing_system7_0_100M_0.xdc]
+  read_xdc /home/INTRA/raogbe/workspace/soc-audio-mixer/ip_repo/zedboard_audio/constraints/zed_audio.xdc
   link_design -top audio_mixer_wrapper -part xc7z020clg484-1
   close_msg_db -file init_design.pb
 } RESULT]
